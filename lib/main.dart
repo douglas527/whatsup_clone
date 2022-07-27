a6523f6b0f4d42b757f0021a95cfa06a356ca9d9
Douglas Kagai <kagaidouglas@gmail.com>
2022-07-27 13:52:57 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:whatsup_clone/screens/mobile_layout_screen.dart';
+
+import 'colors.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Whatsapp UI',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
+      ),
+      home: const MobileLayoutScreen(),
+    );
+  }
+}
