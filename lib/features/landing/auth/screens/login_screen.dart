35c05aeff921a0c5ade225457724ec5c55fbd2fc
Douglas Kagai <kagaidouglas@gmail.com>
2022-07-28 08:09:05 +0300

    authentication functionality

--- lib/features/landing/auth/screens/login_screen.dart
+++ lib/features/landing/auth/screens/login_screen.dart
@@ -1,7 +1,10 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, no_leading_underscores_for_local_identifiers
 
+import 'package:country_picker/country_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsup_clone/colors.dart';
+
+import '../../../../commons/widgets/custom_button.dart';
 
 class LoginScreen extends StatefulWidget {
   static const routeName = '/login-screen';
@@ -12,26 +15,80 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final phoneController = TextEditingController();
+  Country? country;
+  @override
+  void dispose() {
+    super.dispose();
+    phoneController.dispose();
+  }
+
+  void pickCountry() {
+    showCountryPicker(
+        context: context,
+        onSelect: (Country _country) {
+          setState(() {
+            country = _country;
+          });
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: backgroundColor,
         elevation: 0,
         title: const Text('Enter your phone number'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const Text(
-            'WhatsApp will need to verify your phone number',
-            textAlign: TextAlign.center,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(18),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                'WhatsApp will need to verify your phone number',
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextButton(
+                  onPressed: pickCountry, child: const Text('Pick Country')),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  if (country != null) Text('+${country!.phoneCode}'),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  SizedBox(
+                    width: size.width * 0.7,
+                    child: TextField(
+                      controller: phoneController,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        hintText: 'phone number',
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: size.height * 0.6),
+              SizedBox(
+                width: 90,
+                child: CustomButton(
+                  onPressed: () {},
+                  text: 'Next',
+                ),
+              ),
+            ],
           ),
-          const SizedBox(
-            height: 10,
-          ),
-          TextButton(onPressed: () {}, child: const Text('Pick Country')),
-        ],
+        ),
       ),
     );
   }
--- lib/router.dart
+++ lib/router.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:whatsup_clone/commons/widgets/error.dart';
+import 'package:whatsup_clone/features/landing/auth/otp_screen.dart';
 
 import 'features/landing/auth/screens/login_screen.dart';
 
@@ -8,6 +9,13 @@ Route<dynamic> generateRoute(RouteSettings settings) {
     case LoginScreen.routeName:
       return MaterialPageRoute(
         builder: (context) => const LoginScreen(),
+      );
+    case OTPScreen.routeName:
+      final verificationId = settings.arguments as String;
+      return MaterialPageRoute(
+        builder: (context) => OTPScreen(
+          verificationId: verificationId,
+        ),
       );
     default:
       return MaterialPageRoute(
